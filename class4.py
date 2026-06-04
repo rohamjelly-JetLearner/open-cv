cde975227230985fdce42d862bd8a4943bb15be9
rohamjelly-JetLearner <rohamgamer2012@Gmail.com>
2026-06-04 17:59:59 +0100

    Add files via upload

--- class4.py
+++ class4.py
@@ -4,8 +4,33 @@ from PIL import Image
 path=r'C:\Users\Ehsan\OneDrive\Desktop\roham coding\open cv\class file\images'
 os.chdir(r'C:\Users\Ehsan\OneDrive\Desktop\roham coding\open cv\class file\images')
 mwid=0
-mheig=0
+mheight=0
 imgs=[]
 for i in os.listdir('.'):
     if i.endswith(('.webp')):
         imgs.append(i)
+for i in imgs:
+    img=Image.open(os.path.join(path,i))
+    w,h=img.size
+    mwid=mwid+w
+    mheight=mheight+h
+l=len(imgs)
+mwid=mwid//l
+mheight=mheight//l
+print(mwid,mheight)
+for i in imgs:
+    img=Image.open(os.path.join(path,i))
+    imgr=img.resize((mwid,mheight))
+    imgr.save(i,'png',quality=97)
+vid='777777.mp4'
+imgs=[]
+for i in os.listdir('.'):
+    if i.endswith(('.webp')):
+        imgs.append(i)
+print(imgs)
+frame=cv2.imread(os.path.join(path,imgs[0]))
+video=cv2.VideoWriter(vid,cv2.VideoWriter_fourcc(*'mp4v'),0.77777777777,(mwid,mheight))
+for i in imgs:
+    video.write(cv2.imread(os.path.join(path,i)))
+video.release()
+    
