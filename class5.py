2b63ed95c5d838cf021919c70daaddfb8956d5c8
rohamjelly-JetLearner <rohamgamer2012@Gmail.com>
2026-07-09 17:28:22 +0100

    Add files via upload

--- class5.py
+++ class5.py
@@ -12,7 +12,7 @@ bg=np.flip(bg,axis=1)
 while vid.isOpened():
     bv,frames=vid.read()
     frames=np.flip(frames,axis=1)
-    hsv=cv2.cvtColor(frames,cv2.COLOR_RGB2HSV)
+    hsv=cv2.cvtColor(frames,cv2.COLOR_BGR2HSV)
     sr=np.array([0,120,40])
     er=np.array([100,255,255])
     mask=cv2.inRange(hsv,sr,er)
@@ -23,7 +23,9 @@ while vid.isOpened():
     mask3=cv2.morphologyEx(mask2,cv2.MORPH_OPEN,np.ones((3,3),np.uint8),iterations=5)
     mask4=cv2.bitwise_not(mask3)
     result=cv2.bitwise_and(bg,bg,mask=mask3)
-    result1=cv2.bitwise_and(frames,frames,mask4)
+    result1=cv2.bitwise_and(frames,frames,mask=mask4)
     result2=result+result1
     res=cv2.imshow('image',result2)
-
+    key=cv2.waitKey(10)
+    if key==27 or key==32:
+        break
