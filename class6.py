267d6eda05a00210ce78d6a5de839e7eaab01d6e
rohamjelly-JetLearner <rohamgamer2012@Gmail.com>
2026-08-06 18:01:21 +0100

    Add files via upload

--- class6.py
+++ class6.py
@@ -8,4 +8,16 @@ sf=r'C:\Users\Ehsan\OneDrive\Desktop\roham coding\open cv\face recognition\facei
 path=os.path.join(face,sf)
 if not os.path.isdir(path):
     os.mkdir(path)
-    
+cas=cv2.CascadeClassifier(haar)
+cam=cv2.VideoCapture(0)
+for i in range(30):
+    camvalid,camimage=cam.read()
+    # print(camimage)
+    camgrey=cv2.cvtColor(camimage,cv2.COLOR_BGR2GRAY)
+    print(camgrey)
+    rect=cas.detectMultiScale(camgrey,1.2,3)
+    print(rect)
+    # x=rect[0][0]
+    # y=rect[0][1]
+    # width=rect[0][2]
+    # height=rect[0][3]
