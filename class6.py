219b1c9f51589633778ec0f3025e29860237b1eb
rohamjelly-JetLearner <rohamgamer2012@Gmail.com>
2026-08-20 17:27:54 +0100

    Add files via upload

--- class6.py
+++ class6.py
@@ -9,15 +9,18 @@ path=os.path.join(face,sf)
 if not os.path.isdir(path):
     os.mkdir(path)
 cas=cv2.CascadeClassifier(haar)
+print(cas.empty())
 cam=cv2.VideoCapture(0)
 for i in range(30):
     camvalid,camimage=cam.read()
     # print(camimage)
     camgrey=cv2.cvtColor(camimage,cv2.COLOR_BGR2GRAY)
     print(camgrey)
-    rect=cas.detectMultiScale(camgrey,1.2,3)
+    rect=cas.detectMultiScale(camgrey,scaleFactor=1.1,minNeighbors=3,minSize=(30,30))
     print(rect)
     # x=rect[0][0]
     # y=rect[0][1]
     # width=rect[0][2]
     # height=rect[0][3]
+    # cv2.imshow('image',camimage)
+    # cv2.waitKey(0)
